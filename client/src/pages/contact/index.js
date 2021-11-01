93c586f57ed97e5e345932c84ee97a5dfc0d9033
Otávio Pereira <otaviopereira2004@gmail.com>
2021-11-01 05:26:18 -0300

    Envio do form para o servidor

--- client/src/pages/contact/index.js
+++ client/src/pages/contact/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ContactForm from './components/ContactForm';
 
 function Contact(){
     return(
         <div>
             <h1>CONTACT PAGE</h1>
+            <ContactForm />
         </div>
     );
 }
--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
-
 const port = 8888 || process.env.PORT;
 
+app.use(require('cors')());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    //res.json({log: 'Funcionando...'});
+});
+
+app.post('/cadastro', (req, res) => {
+    const user = [];
+
+    console.log('Recebido!')
+    user.push({
+        name: req.body.name,
+        lastName: req.body.lastName
+    });
+    res.json(({message: 'Tá indo...', data: user}));
+});
+
 app.listen(port, () => (
-    `Open in http://localhost:${port}`
+    console.log(`Open in http://localhost:${port}`)
 ));
